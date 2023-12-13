6cab288e4e9cea2beab61d0f920c4063b0dd6943
Leiner <leinersacdalan4@gmail.com>
2023-12-14 02:12:04 +0800

    Teachers

--- src/routes/Teachers.ts
+++ src/routes/Teachers.ts
@@ -9,10 +9,10 @@ router.get('/get/:teacherID', Teachers.readTeacher);
 router.get('/get/', Teachers.readAllTeachers);
 router.put('/update/:teacherID', Teachers.updateTeacher);
 router.delete('/delete/:teacherID', Teachers.deleteTeacher);
-router.put('/update/student/:teacherID/course/:courseID', Teachers.updateCourse);
-router.delete('/delete/student/:teacherID/course/:courseID', Teachers.deleteCourse);
+router.put('/update/teacher/:teacherID/course/:courseID', Teachers.updateCourse);
+router.delete('/delete/teacher/:teacherID/course/:courseID', Teachers.deleteCourse);
 router.post('/add/course/:teacherID', Teachers.addCourse);
-router.get('/get/student/:teacherID/courses', Teachers.readAllCourse);
-router.get('/get/student/:teacherID/course/:courseCode', Teachers.readCourse)
+router.get('/get/teacher/:teacherID/courses', Teachers.readAllCourse);
+router.get('/get/teacher/:teacherID/course/:courseCode', Teachers.readCourse)
 
 export default router;
