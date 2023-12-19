b97a0b39b28d8515c234dd3831b8aed67c78e85a
Leiner <leinersacdalan4@gmail.com>
2023-12-19 13:12:55 +0800

    Schedule

--- src/models/Schedule.ts
+++ src/models/Schedule.ts
@@ -23,27 +23,27 @@ export interface ISchedule extends Document {
   };
 
  /** schema */
-const ScheduleSchema: Schema = new Schema({
-    options: { type: String, required: true },
-    programs: [{
-          _id: { type: String, required: false },
-          program: { type: String, required: true },
-          year: { type: String, required: true },
-          semester: { type: String, required: true },
-          block: { type: String, required: true },
-          sched: [
-            {
-              _id: { type: String, required: false },
-              courseCode: { type: String, required: true },
-              courseDescription: { type: String, required: true },
-              courseUnit: { type: String, required: true },
-              day: { type: String, required: true },
-              time: { type: String, required: true },
-              room: { type: String, required: true },
-              instructor: { type: String, required: true },
-            },
-          ]
-        }]
-  });
+ const ScheduleSchema = new Schema({
+  options: { type: String, required: true },
+  programs: [{
+    _id: { type: Schema.Types.ObjectId, auto: true }, // Specify Schema.Types.ObjectId for automatic generation
+    program: { type: String, required: true },
+    year: { type: String, required: true },
+    semester: { type: String, required: true },
+    block: { type: String, required: true },
+    sched: [
+      {
+        _id: { type: Schema.Types.ObjectId, auto: true }, // Specify Schema.Types.ObjectId for automatic generation
+        courseCode: { type: String, required: true },
+        courseDescription: { type: String, required: true },
+        courseUnit: { type: String, required: true },
+        day: { type: String, required: true },
+        time: { type: String, required: true },
+        room: { type: String, required: true },
+        instructor: { type: String, required: true },
+      },
+    ],
+  }],
+});
   
 export default mongoose.model<ISchedule>('Schedule', ScheduleSchema);
